951225ebd42e178ead865d4b560fb610ff00c104
SylvainDruaux <sydru06@gmail.com>
2024-04-03 11:56:18 +0200

    Initial Commit

---
+++ ImageQuest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageQuest
+//
+//  Created by Sylvain Druaux on 03/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageQuest/ImageQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageQuestApp.swift
+//  ImageQuest
+//
+//  Created by Sylvain Druaux on 03/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
